b45d1aedf6510004948dd8a948f1efdb4074ee77
Aung Soe Oo <aungsoeoo.94s@gmail.com>
2021-01-23 17:44:19 +0630

    change logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => 'လုပ်ငန်းလိုင်စင်',
+    'logo' => 'MOHS(NPW)',
     'logo_img' => 'vendor/adminlte/dist/img/icon.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
